83c047dc1de74a0b038152c182f3b71d5f301be1
Roy Osherove <roy@osherove.com>
2012-04-03 14:20:27 +0200

    ch1 start

---
+++ ArtOfUnitTesting2ndEd.Samples/Chapter1.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter1.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SimpleParserTests.TestReturnsZeroWhenEmptyString();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+    }
+}
---
+++ ArtOfUnitTesting2ndEd.Samples/Chapter1/SimpleParser.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Chapter1
+{
+    public class SimpleParser
+    {
+        public int ParseAndSum(string numbers)
+        {
+            if (numbers.Length == 0)
+            {
+                return 0;
+            }
+            if (!numbers.Contains(","))
+            {
+                return int.Parse(numbers);
+            }
+            else
+            {
+                throw new InvalidOperationException(
+                    "I can only handle 0 or 1 numbers for now!");
+            }
+        }
+
+    }
+
+    public class SimpleParserTests
+    {
+        public static void TestReturnsZeroWhenEmptyString()
+        {
+            try
+            {
+                SimpleParser p = new SimpleParser();
+                int result = p.ParseAndSum(string.Empty);
+                if (result != 0)
+                {
+
+                    Console.WriteLine(
+                        @"***SimpleParserTests.TestReturnsZeroWhenEmptyString: 
+-------
+Parse and sum should have returned 0 on an empty string");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+
+}
