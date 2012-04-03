a13a63a5024735a6d59a3902012cca02fa89b44e
Roy Osherove <roy@osherove.com>
2012-04-03 14:55:37 +0200

    added TestUtil

--- ArtOfUnitTesting2ndEd.Samples/Chapter1.Console/Program.cs
+++ ArtOfUnitTesting2ndEd.Samples/Chapter1.Console/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Chapter1.Console
+namespace Chapter1.ConsoleApp
 {
     class Program
     {
--- ArtOfUnitTesting2ndEd.Samples/Chapter1/SimpleParser.cs
+++ ArtOfUnitTesting2ndEd.Samples/Chapter1/SimpleParser.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 
 namespace Chapter1
 {
@@ -46,5 +47,45 @@ Parse and sum should have returned 0 on an empty string");
             }
         }
     }
+
+    public class TestUtil
+    {
+        public static void ShowProblem(string test, string message)
+        {
+            string msg = string.Format(@"
+---{0}---
+       {1}
+--------------------
+", test, message);
+            Console.WriteLine(msg);
+        }
+    }
+
+    public class SimpleParserTests_WithTestUtil
+    {
+        public static void TestReturnsZeroWhenEmptyString()
+        {
+            string testName = MethodBase.GetCurrentMethod().Name;
+            try
+            {
+                SimpleParser p = new SimpleParser();
+                int result = p.ParseAndSum(string.Empty);
+                if(result!=0)
+                {
+            //Calling the helper method
+                    TestUtil.ShowProblem(testName, 
+"Parse and sum should have returned 0 on an empty string");
+                }
+            }
+            catch (Exception e)
+            {
+                TestUtil.ShowProblem(testName, e.ToString());
+            }
+        }
+
+    }
+
+
+
 
 }
