c1bbcace3e10bed17c5e24d0324c476d4f9de170
Roy Osherove <roy@osherove.com>
2012-04-19 00:41:33 +0200

    more samples for ch2

--- ArtOfUnitTesting2ndEd.Samples/LogAn.UnitTests/LogAnalyzerTests.cs
+++ ArtOfUnitTesting2ndEd.Samples/LogAn.UnitTests/LogAnalyzerTests.cs
@@ -8,7 +8,60 @@ namespace LogAn.UnitTests
         [Test]
         public void IsValidLogFileName_BadExtension_ReturnsFalse()
         {
+            LogAnalyzer analyzer = new LogAnalyzer();
+
+            bool result = analyzer.IsValidLogFileName("filewithbadextension.foo");
+
+            Assert.False(result);
 
         }
+        [Test]
+        public void IsValidLogFileName_GoodExtensionlowercase_ReturnsTrue()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+
+            bool result = analyzer.IsValidLogFileName("filewithgoodextension.slf");
+
+            Assert.True(result);
+
+        }
+
+
+        [Test]
+        public void IsValidLogFileName_GoodExtensionUpperCase_ReturnsTrue()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+
+            bool result = analyzer.IsValidLogFileName("filewithgoodextension.SLF");
+
+            Assert.True(result);
+
+        }
+
+        // this is a refactoring of the previous two tests
+        [TestCase("filewithgoodextension.SLF")]
+        [TestCase("filewithgoodextension.slf")]
+        public void IsValidLogFileName_ValidExtensions_ReturnsTrue(string file)
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+
+            bool result = analyzer.IsValidLogFileName(file);
+
+            Assert.True(result);
+        }
+        
+        // this is a refactoring of all the "regular" tests
+        [TestCase("filewithgoodextension.SLF",true)]
+        [TestCase("filewithgoodextension.slf",true)]
+        [TestCase("filewithbadextension.foo",false)]
+        public void IsValidLogFileName_VariousExtensions_ChecksThem(string file,bool expected)
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+
+            bool result = analyzer.IsValidLogFileName(file);
+
+            Assert.AreEqual(expected,result);
+        }
+
     }
 }
--- ArtOfUnitTesting2ndEd.Samples/LogAn/LogAnalyzer.cs
+++ ArtOfUnitTesting2ndEd.Samples/LogAn/LogAnalyzer.cs
@@ -1,10 +1,13 @@
-﻿namespace LogAn
+﻿using System;
+using System.Globalization;
+
+namespace LogAn
 {
     public class LogAnalyzer
     {
         public bool IsValidLogFileName(string fileName)
         {
-            if (!fileName.EndsWith(".SLF"))
+            if (!fileName.EndsWith(".SLF",StringComparison.CurrentCultureIgnoreCase))
             {
                 return false;
             }
