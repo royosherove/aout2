1bd5cd5a4f0ccfe3111f94a2e5407f8d154810bb
roy osherove <roy@osherove.com>
2012-11-19 00:06:56 +0100

    removed unneeded usings

--- ArtOfUnitTesting2ndEd.Samples/NSubExamples/NSubBasics.cs
+++ ArtOfUnitTesting2ndEd.Samples/NSubExamples/NSubBasics.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Chapter5.LogAn;
+﻿using Chapter5.LogAn;
 using NSubstitute;
 using NUnit.Framework;
 
