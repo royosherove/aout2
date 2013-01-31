a81e6b91f80e3a08b28edf4a2cdd70e886e35601
unknown <roy@osherove.com>
2013-01-31 22:02:23 +0100

    fill in the blanks naming

---
+++ ArtOfUnitTesting2ndEd.Samples/Examples.Tests/Inherited/FillInTheBlanksStringParserTests.cs
@@ -0,0 +1,78 @@
+using System;
+using AOUT.CH6.LogAN;
+using NUnit.Framework;
+
+namespace Examples.Tests.Inherited
+{
+    public abstract class BaseStringParserTests
+    {
+        protected abstract IStringParser GetParser(string input);
+        protected abstract string HeaderVersion_SingleDigit { get; }
+        protected abstract string HeaderVersion_WithMinorVersion { get; }
+        protected abstract string HeaderVersion_WithRevision { get; }
+        public const string EXPECTED_SINGLE_DIGIT = "1";
+        public const string EXPECTED_WITH_REVISION = "1.1.1";
+        public const string EXPECTED_WITH_MINORVERSION = "1.1";
+
+
+        [Test]
+        public void GetStringVersionFromHeader_SingleDigit_Found()
+        {
+            string input = HeaderVersion_SingleDigit;
+            IStringParser parser = GetParser(input);
+
+            string versionFromHeader = parser.GetTextVersionFromHeader();
+            Assert.AreEqual(EXPECTED_SINGLE_DIGIT,versionFromHeader);
+        }
+
+
+        [Test]
+        public void GetStringVersionFromHeader_WithMinorVersion_Found()
+        {
+            string input = HeaderVersion_WithMinorVersion;
+            IStringParser parser = GetParser(input);
+
+            string versionFromHeader = parser.GetTextVersionFromHeader();
+            Assert.AreEqual(EXPECTED_WITH_MINORVERSION,versionFromHeader);
+        }
+
+        [Test]
+        public void GetStringVersionFromHeader_WithRevision_Found()
+        {
+            string input = HeaderVersion_WithRevision;
+            IStringParser parser = GetParser(input);
+
+            string versionFromHeader = parser.GetTextVersionFromHeader();
+            Assert.AreEqual(EXPECTED_WITH_REVISION,versionFromHeader);
+        }
+
+    }
+
+    //An example of the same idea using Generics
+    public abstract class StringParserTests<T>
+        where T:IStringParser
+    {
+        protected T GetParser(string input)
+        {
+            return (T) Activator.CreateInstance(typeof (T), input);
+        }
+
+        [Test]
+        public void GetStringVersionFromHeader_SingleDigit_Found()
+        {
+            string input = "header; \n";
+            T parser = GetParser(input);
+
+            bool result = parser.HasCorrectHeader();
+            Assert.IsFalse(result);
+        }
+
+        //more tests
+        //...
+   }
+    //AN example of a test inheriting from a Generic Base Class
+    [TestFixture]
+    public class StandardStringParserGenericTests
+                        :StringParserTests<StandardStringParser>
+    {}
+}
